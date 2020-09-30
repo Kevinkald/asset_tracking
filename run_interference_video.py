ac11d72d17fe701a2ec212c028dd394f9b5adabe
kevinkald <kevin.k.kaldvansvik@gmail.com>
2020-09-30 17:24:09 +0200

    add video calibration

--- run_interference_video.py
+++ run_interference_video.py
@@ -10,8 +10,8 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 # Load image paths
-VIDEO_PATH = "../../../Data/scenario_1/MOV_0012.MP4"
-
+#VIDEO_PATH = "../../../Data/scenario_1/MOV_0012.MP4"
+VIDEO_PATH = "../../../Data/scenario_2/MOV_0020.MP4"
 # Path to trained model
 PATH_TO_MODEL_DIR = "exported-models/my_ssd_resnet50_v1_fpn/"
 
@@ -45,85 +45,73 @@ def load_image_into_numpy_array(path):
 	return np.array(Image.open(path))
 
 import cv2
-
-cap = cv2.VideoCapture(VIDEO_PATH)
-
-from cv2 import aruco
-
 import apriltag
 
-def draw(img, tags):
-    color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    offset = 75
-
-    for tag in tags:
-        print(f">> {tag.tag_id} {tag.tag_family}")
-        for idx in range(len(tag.corners)):
-            cv2.line(color_img, 
-                     tuple(tag.corners[idx-1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), 
-                     (0, 255, 0),
-                     thickness=4)
-
-        cv2.putText(color_img, str(tag.tag_id),
-                    org=(tag.corners[0, 0].astype(int),tag.corners[0, 1].astype(int)+offset),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=3,
-                    thickness = 6,
-                    color=(0, 0, 255))
-    return color_img
+cap = cv2.VideoCapture(VIDEO_PATH)
+frame = 3800
+cap.set(cv2.CAP_PROP_POS_FRAMES, frame-1)
 
 
-count = 0
+count = frame
 while(cap.isOpened()):
 	print('Running inference for {}... '.format(VIDEO_PATH), end='')
 
 	ret, frame = cap.read()
-
-	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	print("frame: ", count)
+	
+	gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	detector = apriltag.Detector()
-	result = detector.detect(gray)
-
-	cv2.imshow('frame',frame_markers)
-
-	#frame_np = load_image_into_numpy_array(np.array(frame))
+	result = detector.detect(gray_frame)
+	NR_DETECTIONS = len(result)
+	print("# detected fiducials", NR_DETECTIONS)
+	for i in range(NR_DETECTIONS):
+	# We want to draw four lines
+		for j in range(4):
+			start_point = tuple(result[i].corners[j-1, :].astype(int))
+			end_point = tuple(result[i].corners[j, :].astype(int))
+			cv2.line(frame, start_point,
+				end_point, (255, 0, 0),
+				thickness=3)
 
 	# The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-	# input_tensor = tf.convert_to_tensor(frame)
-	# # The model expects a batch of images, so add an axis with `tf.newaxis`.
-	# input_tensor = input_tensor[tf.newaxis, ...]
-	# # input_tensor = np.expand_dims(image_np, 0)
-	# detections = detect_fn(input_tensor)
+	input_tensor = tf.convert_to_tensor(frame)
+	# The model expects a batch of images, so add an axis with `tf.newaxis`.
+	input_tensor = input_tensor[tf.newaxis, ...]
+	# input_tensor = np.expand_dims(image_np, 0)
+	detections = detect_fn(input_tensor)
 
-	# # All outputs are batches tensors.
-	# # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-	# # We're only interested in the first num_detections.
-	# num_detections = int(detections.pop('num_detections'))
-	# detections = {key: value[0, :num_detections].numpy()
-	#                for key, value in detections.items()}
-	# detections['num_detections'] = num_detections
+	# All outputs are batches tensors.
+	# Convert to numpy arrays, and take index [0] to remove the batch dimension.
+	# We're only interested in the first num_detections.
+	num_detections = int(detections.pop('num_detections'))
+	detections = {key: value[0, :num_detections].numpy()
+	               for key, value in detections.items()}
+	detections['num_detections'] = num_detections
 
-	# # detection_classes should be ints.
-	# detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+	# detection_classes should be ints.
+	detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
 
-	# image_np_with_detections = frame.copy()
+	image_np_with_detections = frame.copy()
 
-	# category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
- #                                                                    use_display_name=True)
+	category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
+                                                                    use_display_name=True)
 
-	# viz_utils.visualize_boxes_and_labels_on_image_array(
-	# 	image_np_with_detections,
-	# 	detections['detection_boxes'],
-	# 	detections['detection_classes'],
-	# 	detections['detection_scores'],
-	# 	category_index,
-	# 	use_normalized_coordinates=True,
-	# 	max_boxes_to_draw=10,
-	# 	min_score_thresh=.30,
-	# 	agnostic_mode=False)
+	viz_utils.visualize_boxes_and_labels_on_image_array(
+		image_np_with_detections,
+		detections['detection_boxes'],
+		detections['detection_classes'],
+		detections['detection_scores'],
+		category_index,
+		use_normalized_coordinates=True,
+		max_boxes_to_draw=10,
+		min_score_thresh=.30,
+		agnostic_mode=False)
 
-	# cv2.imshow('frame',image_np_with_detections)
+	cv2.imshow('frame',image_np_with_detections)
 	if cv2.waitKey(2) & 0xFF == ord('q'):
 		break
+
+	count = count + 1
 
 
 
